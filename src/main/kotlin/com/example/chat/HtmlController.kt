224b80cd442b79b5276b0d8e9d6a65eb9e1abcf3
Viktoria <vshkurat24@gmail.com>
2020-04-10 13:59:59 +0200

    Added Html Controller and Mustache Templates

---
+++ src/main/kotlin/com/example/chat/HtmlController.kt
@@ -0,0 +1,23 @@
+package com.example.chat
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+
+    /*
+    * @GetMapping annotation maps HTTP GET requests onto specific handler methods.
+    * It is a composed annotation that acts as a shortcut for
+    * @RequestMapping(method = RequestMethod.GET).
+    * */
+
+    @GetMapping("/")
+    fun chat(model: Model) :String{
+        model["title"] = "Blog"
+        return "blog"
+    }
+}
